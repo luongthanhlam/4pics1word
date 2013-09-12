c7898c76093dfc3f33e2becb916a92f949bd4894
saber <saber@saber-PC>
2013-09-12 22:02:01 +0700

    Updated

---
+++ src/com/example/Entity/Solution.java
@@ -0,0 +1,84 @@
+package com.example.Entity;
+
+public class Solution {
+	private char solution;
+	private char answer;
+	private boolean reveal = false;
+	private boolean remove = true;
+	private int tag;
+
+	/**
+	 * @return the solution
+	 */
+	public char getSolution() {
+		return solution;
+	}
+
+	/**
+	 * @param solution
+	 *            the solution to set
+	 */
+	public void setSolution(char solution) {
+		this.solution = solution;
+	}
+
+	/**
+	 * @return the answer
+	 */
+	public char getAnswer() {
+		return answer;
+	}
+
+	/**
+	 * @param answer
+	 *            the answer to set
+	 */
+	public void setAnswer(char answer) {
+		this.answer = answer;
+	}
+
+	/**
+	 * @return the reveal
+	 */
+	public boolean isReveal() {
+		return reveal;
+	}
+
+	/**
+	 * @param reveal
+	 *            the reveal to set
+	 */
+	public void setReveal(boolean reveal) {
+		this.reveal = reveal;
+	}
+
+	/**
+	 * @return the tag
+	 */
+	public int getTag() {
+		return tag;
+	}
+
+	/**
+	 * @param tag
+	 *            the tag to set
+	 */
+	public void setTag(int tag) {
+		this.tag = tag;
+	}
+
+	/**
+	 * @return the remove
+	 */
+	public boolean isRemove() {
+		return remove;
+	}
+
+	/**
+	 * @param remove the remove to set
+	 */
+	public void setRemove(boolean remove) {
+		this.remove = remove;
+	}
+
+}
---
+++ src/com/example/Entity/Suggest.java
@@ -0,0 +1,5 @@
+package com.example.Entity;
+
+public class Suggest {
+
+}
